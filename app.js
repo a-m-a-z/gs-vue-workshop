7d83216208b3be2a011b1575febf238d30411d82
Christian Nwamba <nwambachristian@gmail.com>
2017-09-12 16:12:53 +0100

    Helper update counter

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const decrement = controls[0];
 const increment = controls[1];
 
 increment.addEventListener('click', function() {
-  console.log(counter);
-  let counterInt = parseInt(counter.innerText);
-  console.log(counterInt)
-  counterInt = counterInt + 1;
-  counter.innerText = counterInt;
+  updateCounter(+1)
 })
 
 decrement.addEventListener('click', function() {
-  let counterInt = parseInt(counter.innerText);
-  counterInt = counterInt - 1;
-  counter.innerText = counterInt;
+  updateCounter(-1)
 })
+
+function updateCounter(num) {
+  let counterInt = parseInt(counter.innerText);
+  counterInt = counterInt + num;
+  counter.innerText = counterInt;
+}
