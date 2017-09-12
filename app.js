6cfc8a620d046821998cac54684782b1565b4d17
Christian Nwamba <nwambachristian@gmail.com>
2017-09-12 20:36:51 +0100

    Cross off todos

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ new Vue({
   methods: {
     addTask: function() {
       this.todos.push({text: this.text, completed: false});
+    },
+    toggleTodo: function(todoToUpdate) {
+      this.todos = this.todos.map(todo => {
+        if(todo.text !== todoToUpdate.text) {
+          return todo;
+        }
+        const update = {
+          text: todo.text,
+          completed: !todo.completed
+        }
+        return update;
+      })
     }
   }
 })
